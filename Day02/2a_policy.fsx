5ed1a0a8e3d5315cc09ad8f77869072046ad78ba
Chris J <cej@nightwolf.org.uk>
2020-12-07 21:53:14 +0000

    Comments for my own notes

--- Day02/2a_policy.fsx
+++ Day02/2a_policy.fsx
@@ -8,6 +8,7 @@ type pwinfo = {
     passwd: string
 }
 
+// Cribbed and tweaked from MSDN. Need to better understand Active Patterns.
 let (|PwInfo|) input =
     // Line format is min-max char: passwd
     let m = Regex.Match(input, "^(\d+)-(\d+) ([a-z]): (.*)$")
@@ -17,6 +18,7 @@ let lines = [
     use sr = new StreamReader(@"input.txt")
     while not sr.EndOfStream do
         let line = sr.ReadLine()
+        // So Active Patterns mean we can use a regex and split a string into seperate vars
         match line with
             | PwInfo [minc; maxc; c; passwd] -> { 
                     minc = minc |> int
@@ -30,6 +32,7 @@ let lines = [
 let counts = seq {
     for p in lines do
         let cnt = 
+            // Oh nice, a string in F# can be naturally decomposed to a sequence of chars
             p.passwd 
             |> Seq.filter (fun c -> c = p.ch) 
             |> Seq.length
