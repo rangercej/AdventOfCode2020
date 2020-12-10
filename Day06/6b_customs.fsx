11730e0fb7a1cabd74cbcd3fc5d517d04b3496d5
Chris J <cej@nightwolf.org.uk>
2020-12-10 20:03:52 +0000

    Performance; treat each group as a native array rather than turning into a string and then splitting again!

--- Day06/6b_customs.fsx
+++ Day06/6b_customs.fsx
@@ -1,27 +1,26 @@
 #time "on"
 
-// Read each group and merge into a single line.
+// Read each group into an array-per-line and add to the list
 let lines = [
     use sr = new System.IO.StreamReader(@"input.txt")
     let input = new System.Collections.Generic.List<string>()
     while not sr.EndOfStream do
         let l = sr.ReadLine()
         if l = "" then
-            System.String.Join(";", input)
+            input |> Array.ofSeq
             input.Clear()
         else
             input.Add(l)
 
     // Add the tail of the file (assuming not empty)
     if input.Count > 0 then
-        System.String.Join(";", input)
+        input |> Array.ofSeq
 ]
 
-// Each group is a single line, with each person's answers sepearated by ';'. So
-// split to get an array of each person's answers, then cast each element of the
-// array to a set of individual characters, then call intersectMany.
-let findIntersect (s:string) =
-    s.Split(';') 
+// For each string in the array, turn it into a set of chars so we
+// have an array of sets. Then call intersectMany across them all.
+let findIntersect (answers:array<string>) =
+    answers 
         |> Array.map (fun x -> x.ToCharArray() |> Set.ofArray) 
         |> Set.intersectMany
     
