a7ae509cd25cdb2d99fd766ceffd8757596bc04a
Chris J <cej@nightwolf.org.uk>
2020-12-10 18:29:05 +0000

    Day 5, part 2 complete

---
+++ Day05/5b_seating.fsx
@@ -0,0 +1,42 @@
+#time "on"
+
+let lines = [
+    use sr = new System.IO.StreamReader(@"input.txt")
+    while not sr.EndOfStream do
+        sr.ReadLine()
+]
+
+let rec findNumber (s:list<char>, lower:int, upper:int, fwd:char, bck:char) =
+    if s.Length = 0 then
+        (lower + upper) / 2
+    else
+        let dir = s.[0]
+        let searchSize = 1 + upper - lower 
+        let halfWay = searchSize / 2
+        if dir = fwd then
+            findNumber (s |> List.tail, lower, upper - halfWay, fwd, bck)
+        elif dir = bck then
+            findNumber ((s |> List.tail), lower + halfWay, upper, fwd, bck)
+        else
+            -1
+        
+let getRowMap ptr =
+    let seatPtrs = ptr |> Seq.take 7 |> Seq.toList
+    findNumber (seatPtrs, 0, 127, 'F', 'B')
+
+let getSeatMap ptr =
+    let seatPtrs = ptr |> Seq.skip 7 |> Seq.take 3 |> Seq.toList
+    findNumber (seatPtrs, 0, 7, 'L', 'R')
+
+let seatLoc = lines |> List.map (fun s -> getRowMap s, getSeatMap s)
+let seatIds = seatLoc |> List.map(fun x -> (fst(x) * 8) + snd(x)) |> List.sort |> set
+
+let seatMax = seatIds |> Set.maxElement
+let seatMin = seatIds |> Set.minElement
+let allSeats = [ seatMin .. seatMax ] |> set
+
+// F# sets - we take one set away from the other to find the non-matching elements
+let freeSeats = allSeats - seatIds
+
+printfn "Free seat = %A" freeSeats
+
